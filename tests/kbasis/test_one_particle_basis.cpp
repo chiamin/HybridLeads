a1eca75eaa3a9c066e75fc921a83d4892088101b
taolin <tanlin2013@gmail.com>
2023-01-05 18:24:14 +0800

    Refactor: mv func ALLCLOSE to module

--- tests/kbasis/test_one_particle_basis.cpp
+++ tests/kbasis/test_one_particle_basis.cpp
@@ -4,36 +4,12 @@
 #include <catch2/catch_test_macros.hpp>
 #include <catch2/trompeloeil.hpp>
 
+#include "hybridbasis/utils.h"
 #include "itensor/all.h"
 #include "kbasis/OneParticleBasis.h"
 
 using namespace itensor;
 using namespace Catch;
-
-/**
- * @brief Element-wise comparison of 2 given itensors.
- *
- * @param t1 The first tensor.
- * @param t2 The second tensor.
- * @param atol The absolute tolerance. Default to 1e-12.
- * @return bool
- */
-bool ALLCLOSE(ITensor t1, ITensor t2, double atol = 1e-12) {
-  REQUIRE(order(t1) == order(t2));
-  auto check_close_zero = [&atol](Real r) {
-    if (abs(r) > atol) {
-      throw std::logic_error("Two tensors are not close.");
-    }
-  };
-  try {
-    t2.replaceInds(inds(t2), inds(t1));
-    auto diff_t = t1 - t2;
-    diff_t.visit(check_close_zero);  // visit() only accepts lambda func
-  } catch (std::logic_error& e) {
-    return false;
-  }
-  return true;
-}
 
 TEST_CASE("Check matrix elements of tight-binding Hamiltonian",
           "[tight_binding_Hamilt]") {
