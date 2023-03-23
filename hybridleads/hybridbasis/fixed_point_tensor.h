1a92561f65f4e7502a6d567bc25000d208b8da26
taolin <tanlin2013@gmail.com>
2023-03-23 13:54:47 +0800

    refactor: rm glog init in class

--- hybridleads/hybridbasis/fixed_point_tensor.h
+++ hybridleads/hybridbasis/fixed_point_tensor.h
@@ -22,8 +22,10 @@ class FixedPointTensor {
    * @throws `std::invalid_argument` when `uniform_site` does not give any
    * identical MPO tensors as its neighbour.
    */
-  FixedPointTensor(itensor::MPO const& mpo, int uniform_site,
-                   itensor::Args const& args = itensor::Args::global()) {
+  FixedPointTensor(
+      itensor::MPO const& mpo, int uniform_site,
+      itensor::Args const& args = itensor::Args::global()
+  ) {
     mpo_ = mpo;
     uniform_site_ = uniform_site;
     mpo_checker();
@@ -35,10 +37,10 @@ class FixedPointTensor {
     itensor::Real ortho_tol = args.getReal("ortho_tol", 1e-12);
     int ortho_max_iter = args.getInt("ortho_max_iter", 20);
     RandGen::SeedType seed = args.getInt("seed", 0);
-    FLAGS_logtostderr = args.getBool("log_to_std", true);
-    google::InitGoogleLogging("fixed_point_tensor");
-    itdvp_routine(time_steps, dt, max_bond_dim, tdvp_tol, tdvp_max_iter,
-                  ortho_tol, ortho_max_iter, seed);
+    itdvp_routine(
+        time_steps, dt, max_bond_dim, tdvp_tol, tdvp_max_iter, ortho_tol,
+        ortho_max_iter, seed
+    );
   }
 
   itensor::ITensor get(std::string side) {
@@ -49,52 +51,60 @@ class FixedPointTensor {
  protected:
   itensor::MPO mpo_;
   int uniform_site_;
-  itensor::Index left_link_, right_link_, phys_bond_;
+  itensor::Index mpo_left_idx_, mpo_right_idx_, phys_idx_;
   itensor::ITensor left_fixpt_tensor_, right_fixpt_tensor_;
   itensor::Real en_, err_;
 
   void mpo_checker() {
     int neighbour_site;
-    if (order(mpo_(uniform_site_ - 1)) == order(mpo_(uniform_site_))) {
+    if (itensor::order(mpo_(uniform_site_ - 1)) ==
+        itensor::order(mpo_(uniform_site_))) {
       neighbour_site = uniform_site_ - 1;
-    } else if (order(mpo_(uniform_site_)) == order(mpo_(uniform_site_ + 1))) {
+    } else if (itensor::order(mpo_(uniform_site_)) == itensor::order(mpo_(uniform_site_ + 1))) {
       neighbour_site = uniform_site_ + 1;
     } else {
       throw std::invalid_argument(
-          "Cannot find any neighbouring sites with same MPO tensor order.");
+          "Cannot find any neighbouring sites with same MPO tensor order."
+      );
     }
     if (!ALLCLOSE(mpo_(uniform_site_), mpo_(neighbour_site))) {
       throw std::invalid_argument(
           "The `uniform site` should be picked from the bulk, with at least "
           "one neighbouring MPO tensor being identical with MPO tensor on this "
-          "site.");
+          "site."
+      );
     }
   }
 
   void get_indices() {
-    left_link_ = commonIndex(mpo_(uniform_site_ - 1), mpo_(uniform_site_));
-    right_link_ = commonIndex(mpo_(uniform_site_), mpo_(uniform_site_ + 1));
-    phys_bond_ = findIndex(mpo_(uniform_site_), "Site,0");
+    mpo_left_idx_ = commonIndex(mpo_(uniform_site_ - 1), mpo_(uniform_site_));
+    mpo_right_idx_ = commonIndex(mpo_(uniform_site_), mpo_(uniform_site_ + 1));
+    phys_idx_ = findIndex(mpo_(uniform_site_), "Site,0");
   }
 
-  void itdvp_routine(int time_steps, itensor::Real dt, int max_bond_dim,
-                     itensor::Real tdvp_tol, int tdvp_max_iter,
-                     itensor::Real ortho_tol, int ortho_max_iter,
-                     RandGen::SeedType seed) {
+  void itdvp_routine(
+      int time_steps, itensor::Real dt, int max_bond_dim, itensor::Real tdvp_tol,
+      int tdvp_max_iter, itensor::Real ortho_tol, int ortho_max_iter,
+      RandGen::SeedType seed
+  ) {
     get_indices();
     auto impo = mpo_(uniform_site_);
     auto imps = ITensor();  // ill-defined tensor
-    auto [imps_left, imps_right, imps_left_center, imps_center, imps_left_idty,
-          imps_right_idty] =
-        itdvp_initial(impo, phys_bond_, left_link_, right_link_, imps,
-                      max_bond_dim, ortho_tol, ortho_max_iter, seed);
+    auto
+        [imps_left, imps_right, imps_left_center, imps_center, imps_left_idty,
+         imps_right_idty] =
+            itdvp_initial(
+                impo, phys_idx_, mpo_left_idx_, mpo_right_idx_, imps, max_bond_dim,
+                ortho_tol, ortho_max_iter, seed
+            );
     itensor::Args args = {"ErrGoal=", tdvp_tol, "MaxIter", tdvp_max_iter};
     for (int i = 1; i <= time_steps; i++) {
-      std::tie(en_, err_, left_fixpt_tensor_, right_fixpt_tensor_) =
-          itdvp(impo, imps_left, imps_right, imps_left_center, imps_center,
-                imps_left_idty, imps_right_idty, dt, args);
-      LOG(INFO) << std::printf("In time step %o, energy, error = %.3e, %.3e\n",
-                               i, en_, err_);
+      std::tie(en_, err_, left_fixpt_tensor_, right_fixpt_tensor_) = itdvp(
+          impo, imps_left, imps_right, imps_left_center, imps_center, imps_left_idty,
+          imps_right_idty, dt, args
+      );
+      LOG(INFO
+      ) << std::printf("In time step %o, energy, error = %.3e, %.3e\n", i, en_, err_);
       if (args.getReal("ErrGoal") > tdvp_tol) {
         args.add("ErrGoal=", err_ * 0.1);
       }
